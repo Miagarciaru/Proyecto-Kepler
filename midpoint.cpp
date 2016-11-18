36be615c0e7df01ea461ce73c1ac1bd2f2996fb6
Maria Montes <mpmontesb@unal.edu.co>
2016-11-17 22:25:07 -0500

    Problema resuelto con error en H y L

---
+++ midpoint.cpp
@@ -0,0 +1,61 @@
+// Implicit Midpoint Method
+#include <iostream>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+
+// Declaración de funciones
+void  Hamiltonian (double q1, double q2, double  p1, double p2);
+double f1 (double q1, double q2);
+double f2 (double q1, double q2);
+void Midpoint (double t, double h, double & q1, double & q2, double & p1, double & p2);
+
+int main ()
+{
+  const double h = 0.0005;
+  const double e = 0.6;
+  double q1, q2, p1, p2, H, L, t;
+  q1 = 1-e;
+  p1 = 0;
+  q2 = 0;
+  p2 = std::sqrt((1+e)/(1-e));
+  H = -1/2;
+  L = std::sqrt(1-(e*e));
+  for (t = 0; t <= 20*PI; t += h) {
+    std::cout << t << "\t" << q1 << "\t" << q2 << "\t" << H << "\t" << L << std:: endl;
+    Midpoint(t, h, q1, q2, p1, p2);
+    Hamiltonian(q1, q2, p1, p2);
+  } 
+  return 0;
+}
+
+void  Hamiltonian (double q1, double q2, double  p1, double p2)
+{
+  double H, L;
+  H = ((1/2)*(p1*p1 + p2*p2)) - (1/std::sqrt(q1*q1 + q2*q2));
+  L = q1*p2 - q2*p1;  
+}
+
+double f1 (double t, double q1, double q2)
+{
+  return -q1 / std::pow((q1*q1)+(q2*q2), 3/2);   
+}
+
+double f2 (double t, double q1, double q2)
+{
+  return -q2 / std::pow((q1*q1)+(q2*q2), 3/2);
+}
+
+// Método implícito del punto medio 
+void Midpoint (double t, double h, double & q1, double & q2, double & p1, double & p2)
+{
+  double q10, q20, q1m, q2m;
+  q10 = q1;
+  q20 = q2;
+  q1 = q1 + h*p1;
+  q2 = q2 + h*p2;
+  q1m = (q10+q1)/2;
+  q2m = (q20+q2)/2;
+  p1 = p1 + h*f1(t + h/2, q1m, q2m);
+  p2 = p2 + h*f2(t + h/2, q1m, q2m);
+}
