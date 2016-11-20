beb6c9519033369c090754a5d01368011725a943
Maria Montes <mpmontesb@unal.edu.co>
2016-11-19 23:52:29 -0500

    Método Euler más corto

--- euler.cpp
+++ euler.cpp
@@ -4,6 +4,8 @@
 
 const double PI = 3.14159265358979323846;
 const int N = 2; // Dos dimensiones
+const double h = 0.0005;
+const double e = 0.6;
 
 // Declaración de funciones
 double f (double t, double q[N], int ii);
@@ -11,13 +13,9 @@ void Euler (double t, double h, double (&q)[N], double (&p)[N]);
 
 int main ()
 {
-  const double h = 0.0005;
-  const double e = 0.6;
-  double q[N], p[N], H, L, t;
-  q[0] = 1.0-e;
-  p[0] = 0;
-  q[1] = 0;
-  p[1] = std::sqrt((1.0+e)/(1.0-e));
+  double H, L, t;
+  double q[N] = {1.0-e, 0};
+  double p[N] = {0, std::sqrt((1.0+e)/(1.0-e))};
   for (t = 0; t <= 20.0*PI; t += h) {
     H = (1.0/2.0)*(p[0]*p[0] + p[1]*p[1]) - 1.0/std::sqrt(q[0]*q[0] + q[1]*q[1]);
     L = (q[0]*p[1]) - (q[1]*p[0]);
